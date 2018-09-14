fba7f1d084a5d6aa6009f8b1fc7f9dc4733b864d
최준영 <chad1994@skuniv.ac.kr>
2018-09-14 21:42:34 +0900

    #125 chagne google map to tmap

---
+++ app/src/main/java/com/depromeet/hanriver/hanrivermeetup/fragment/meeting/map/MapFragment.java
@@ -0,0 +1,109 @@
+//package com.depromeet.hanriver.hanrivermeetup.fragment.meeting.map;
+//
+//
+//import android.Manifest;
+//import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//import com.depromeet.hanriver.hanrivermeetup.R;
+//import com.google.android.gms.maps.CameraUpdateFactory;
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.MapView;
+//import com.google.android.gms.maps.MapsInitializer;
+//import com.google.android.gms.maps.OnMapReadyCallback;
+//import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+//import com.google.android.gms.maps.model.CameraPosition;
+//import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.MapStyleOptions;
+//import com.google.android.gms.maps.model.MarkerOptions;
+//import com.gun0912.tedpermission.PermissionListener;
+//import com.gun0912.tedpermission.TedPermission;
+//
+//import java.util.ArrayList;
+//
+//public class MapFragment extends Fragment {
+//    MapView mMapView;
+//    private GoogleMap googleMap;
+//
+//    public MapFragment() {
+//        // Required empty public constructor
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//
+//        View view = inflater.inflate(R.layout.fragment_map, container, false);
+//
+//        //Permission Check
+//        PermissionListener permissionlistener = new PermissionListener() {
+//            @Override
+//            public void onPermissionGranted() {
+//                mMapView = (MapView) view.findViewById(R.id.mapView);
+//                mMapView.onCreate(savedInstanceState);
+//
+//                mMapView.onResume(); // needed to get the map to display immediately
+//
+//                try {
+//                    MapsInitializer.initialize(getActivity().getApplicationContext());
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//
+//                mMapView.getMapAsync(new OnMapReadyCallback() {
+//                    @Override
+//                    public void onMapReady(GoogleMap mMap) {
+//                        googleMap = mMap;
+//
+//                        // For showing a move to my location button
+//                        googleMap.setMyLocationEnabled(true);
+//
+//                        googleMap.setMapStyle(
+//                                MapStyleOptions.loadRawResourceStyle(
+//                                        getActivity().getApplicationContext(), R.raw.google_map_style));
+//
+//                        LatLng sydney = new LatLng(37.527907, 126.934594);
+//                        googleMap.addMarker(new MarkerOptions().position(sydney).title("쓰레기통").snippet("쓰레기를 버려주세요"));
+//
+//                        googleMap.addMarker(new MarkerOptions()
+//                                .position(new LatLng(37.528202, 126.933958))
+//                                .title("쓰레기통")
+//
+//                                .snippet("쓰레기를 버려주세요"));
+//
+//                        googleMap.addMarker(new MarkerOptions()
+//                                .position(new LatLng(37.527722, 126.934178))
+//                                .title("쓰레기통")
+//                                .snippet("쓰레기를 버려주세요"));
+//
+//                        // For zooming automatically to the location of the marker
+//                        CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(17).build();
+//                        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//                    }
+//                });
+//            }
+//
+//            @Override
+//            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+//                // Toast.makeText(MainActivity.this, "권한 거부\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        };
+//        TedPermission.with(getActivity())
+//                .setPermissionListener(permissionlistener)
+//                .setRationaleMessage("어디갈강을 보기위해선 권한을 설정해주셔야합니다!")
+//                .setDeniedMessage("왜 거부하셨어요...\n하지만 [설정] > [권한] 에서 권한을 허용할 수 있어요!")
+//                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+//                .setPermissions(Manifest.permission.ACCESS_COARSE_LOCATION)
+//                .check();
+//
+//        return view;
+//    }
+//}
---
+++ app/src/main/java/com/depromeet/hanriver/hanrivermeetup/fragment/meeting/map/TmapFragment.java
@@ -0,0 +1,48 @@
+package com.depromeet.hanriver.hanrivermeetup.fragment.meeting.map;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.depromeet.hanriver.hanrivermeetup.R;
+import com.skt.Tmap.TMapView;
+
+public class TmapFragment extends Fragment {
+
+    RelativeLayout map;
+    TMapView mapView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        map = view.findViewById(R.id.mapView);
+        mapView = new TMapView(getActivity());
+        mapView.setSKTMapApiKey("api key");
+
+        mapView.setLocationPoint(126.970325,37.556152);
+        mapView.setCenterPoint(126.970325,37.556152);
+        mapView.setCompassMode(false);
+        mapView.setIconVisibility(true);
+        mapView.setZoomLevel(15);
+        mapView.setMapType(TMapView.MAPTYPE_STANDARD);  //일반지도
+        mapView.setLanguage(TMapView.LANGUAGE_KOREAN);
+        mapView.setTrackingMode(false);
+        mapView.setSightVisible(false);
+        map.addView(mapView);
+
+        return view;
+    }
+}
