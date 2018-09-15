c52b08e0ba12d8f381eb64c7ad22ebdce4b28607
Unknown <kimts2002kr@naver.com>
2018-09-15 22:25:56 +0900

    #117 firebase 관련 라이브러리 및 상속 클래스 추가
    * FCM 을 위한 클래스 구현 및 관련 라이브러리 추가

---
+++ app/src/main/java/com/depromeet/hanriver/hanrivermeetup/firebase/CustomFirebaseInstanceIDService.java
@@ -0,0 +1,16 @@
+package com.depromeet.hanriver.hanrivermeetup.firebase;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class CustomFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //FCM 토큰 갱신
+    }
+}
