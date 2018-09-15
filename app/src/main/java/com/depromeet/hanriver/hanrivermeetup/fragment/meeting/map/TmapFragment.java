3f03992dd28db2594dd7c6eee3f1f366e3b09e74
최준영 <chad1994@skuniv.ac.kr>
2018-09-16 02:43:09 +0900

    #127 modify validate contents of createroom

--- app/src/main/java/com/depromeet/hanriver/hanrivermeetup/fragment/meeting/map/TmapFragment.java
+++ app/src/main/java/com/depromeet/hanriver/hanrivermeetup/fragment/meeting/map/TmapFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
+import com.depromeet.hanriver.hanrivermeetup.BuildConfig;
 import com.depromeet.hanriver.hanrivermeetup.R;
 import com.skt.Tmap.TMapView;
 
@@ -30,7 +31,7 @@ public class TmapFragment extends Fragment {
 
         map = view.findViewById(R.id.mapView);
         mapView = new TMapView(getActivity());
-        mapView.setSKTMapApiKey("api key");
+        mapView.setSKTMapApiKey(BuildConfig.TMapApiKey);
 
         mapView.setLocationPoint(126.970325,37.556152);
         mapView.setCenterPoint(126.970325,37.556152);
